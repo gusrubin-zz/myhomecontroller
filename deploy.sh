15f7524e6c34688394cded911c5ae3d72d797ab6
Gustavo Rubin <gusrubin@gmail.com>
2020-05-11 11:32:33 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 
 echo "Deploy last version of MyHome Controller"
 
-rm -rf /opt/myhome
-mkdir /opt/myhome
-tar -zcpvf /tmp/myhome-1.0.tgz *
-tar -zxpvf /tmp/myhome-1.0.tgz --directory=/opt/myhome
+rm -rf /opt/myhomecontroller
+mkdir /opt/myhomecontroller
+tar -zcpvf /tmp/myhomecontroller-1.0.tgz *
+tar -zxpvf /tmp/myhomecontroller-1.0.tgz --directory=/opt/myhomecontroller
 
 echo "Deploy done."
